90798cd41117632ce6da952acdc4e100e06187ab
ankit <devaniankit1990@gmail.com>
2022-11-14 15:55:24 +0530

    setup gh-pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,22 +5,22 @@ const Contact = () => {
 
 <>
 
-<div class="w-full h-screen px-10 mx-auto bg-gradient-to-b from-black to-gray-800 p-4 text-white"   name="contact">
+<div className="w-full h-screen px-10 mx-auto bg-gradient-to-b from-black to-gray-800 p-4 text-white"   name="contact">
 
 <div className="pb-10 mx-5">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Connect With Me
           </p>
 </div>
-  <section class="mb-32 text-gray-800">
+  <section className="mb-32 text-gray-800">
 
 
 
-    <div class="flex flex-wrap">
-      <div class="grow-0 shrink-0 basis-auto mb-12 lg:mb-0 w-full lg:w-5/12 px-3 lg:px-6">
+    <div className="flex flex-wrap">
+      <div className="grow-0 shrink-0 basis-auto mb-12 lg:mb-0 w-full lg:w-5/12 px-3 lg:px-6">
         <form>
-          <div class="form-group mb-6">
-            <input type="text" class="form-control block
+          <div className="form-group mb-6">
+            <input type="text" className="form-control block
             w-full
             px-3
             py-1.5
@@ -36,8 +36,8 @@ const Contact = () => {
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
               placeholder="Name"/>
           </div>
-          <div class="form-group mb-6">
-            <input type="email" class="form-control block
+          <div className="form-group mb-6">
+            <input type="email" className="form-control block
             w-full
             px-3
             py-1.5
@@ -53,8 +53,8 @@ const Contact = () => {
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
               placeholder="Email address"/>
           </div>
-          <div class="form-group mb-6">
-            <textarea class="
+          <div className="form-group mb-6">
+            <textarea className="
             form-control
             block
             w-full
@@ -73,7 +73,7 @@ const Contact = () => {
           " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
           </div>
 
-          <button type="submit" class="
+          <button type="submit" className="
           w-full
           px-6
           py-2.5
@@ -93,67 +93,67 @@ const Contact = () => {
           ease-in-out">Send</button>
         </form>
       </div>
-      <div class="grow-0 shrink-0 basis-auto w-full lg:w-7/12">
-        <div class="flex flex-wrap">
-          <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-            <div class="flex items-start">
-              <div class="shrink-0">
-                <div class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" class="w-5 text-white"
+      <div className="grow-0 shrink-0 basis-auto w-full lg:w-7/12">
+        <div className="flex flex-wrap">
+          <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+            <div className="flex items-start">
+              <div className="shrink-0">
+                <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" className="w-5 text-white"
                     role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M0 32L144 0l80 144-83.8 67c36.1 68.4 92.3 124.6 160.8 160.8L368 288l144 80L480 512H448C200.6 512 0 311.4 0 64L0 32z"/>
           
                   </svg>
                 </div>
               </div>
-              <div class="grow ml-6">
-                <p class="font-bold mb-1 text-white">Phone</p>
-                <p class="text-white">+91 8866556966</p>
+              <div className="grow ml-6">
+                <p className="font-bold mb-1 text-white">Phone</p>
+                <p className="text-white">+91 8866556966</p>
               </div>
             </div>
           </div>
-          <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-            <div class="flex items-start">
-              <div class="shrink-0">
-                <div class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+          <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+            <div className="flex items-start">
+              <div className="shrink-0">
+                <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                   {/* <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="dollar-sign"
-                    class="w-3 text-white" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512"> */}
+                    className="w-3 text-white" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512"> */}
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/> </svg>
                   
                 </div>
                 
               </div>
-              <div class="grow ml-6">
-                <p class="font-bold mb-1 text-white">Instagram</p>
-                <p class="text-white">@ankit_devani.17</p>
+              <div className="grow ml-6">
+                <p className="font-bold mb-1 text-white">Instagram</p>
+                <p className="text-white">@ankit_devani.17</p>
               </div>
             </div>
           </div>
-          <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-            <div class="flex align-start">
-              <div class="shrink-0">
-                <div class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+          <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+            <div className="flex align-start">
+              <div className="shrink-0">
+                <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 128C0 92.65 28.65 64 64 64H448C483.3 64 512 92.65 512 128V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V128zM48 128V150.1L220.5 291.7C241.1 308.7 270.9 308.7 291.5 291.7L464 150.1V127.1C464 119.2 456.8 111.1 448 111.1H64C55.16 111.1 48 119.2 48 127.1L48 128zM48 212.2V384C48 392.8 55.16 400 64 400H448C456.8 400 464 392.8 464 384V212.2L322 328.8C283.6 360.3 228.4 360.3 189.1 328.8L48 212.2z"/></svg>
                 </div>
               </div>
-              <div class="grow ml-6">
-                <p class="font-bold mb-1 text-white">Email</p>
+              <div className="grow ml-6">
+                <p className="font-bold mb-1 text-white">Email</p>
          
-                <p class="text-white">devaniankit1990@gmail.com</p>
+                <p className="text-white">devaniankit1990@gmail.com</p>
               </div>
             </div>
           </div>
-          <div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
-            <div class="flex align-start">
-              <div class="shrink-0">
-                <div class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+          <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+            <div className="flex align-start">
+              <div className="shrink-0">
+                <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
                 </div>
               </div>
-              <div class="grow ml-6">
-                <p class="font-bold mb-1 text-white">Whatsapp</p>
+              <div className="grow ml-6">
+                <p className="font-bold mb-1 text-white">Whatsapp</p>
           
-                <p class="text-white">+91 8866556966</p>
+                <p className="text-white">+91 8866556966</p>
               </div>
             </div>
           </div>
